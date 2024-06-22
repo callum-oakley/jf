2f286dc0f6ebbdd55283eecaeb39b057482aa85c
Callum Oakley <hello@callumoakley.net>
2024-06-22 14:17:36 +0100

    version flag

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { parseArgs } from "jsr:@std/cli@^0.224.5";
 import { toText } from "jsr:@std/streams@^0.224.5";
+import pkg from "./deno.json" with { type: "json" };
 
 const help = `
 Evaluate the given JavaScript and print its completion value
@@ -10,24 +11,28 @@ Options:
   -i, --input      Read STDIN as text and store it in the $ variable
   -p, --parse      Parse STDIN as JSON and store it in the $ variable
   -s, --stringify  JSON.stringify the completion value before printing it
+  -v, --version    Print version and exit
   -h, --help       Print this help message
 
 Defaults:
   All options default to false. The script defaults to $.
 `.trim();
 
-const args = parseArgs(Deno.args, {
-    boolean: ["input", "parse", "stringify", "help"],
-    alias: {
-        input: "i",
-        parse: "p",
-        stringify: "s",
-        help: "h",
-    },
-});
+const flags = ["input", "parse", "stringify", "version", "help"];
+const alias = {};
+for (const flag of flags) {
+    alias[flag] = flag[0];
+}
+
+const args = parseArgs(Deno.args, { boolean: flags, alias });
 
 if (args.help || Deno.args.length == 0) {
     console.log(help);
+    Deno.exit();
+}
+
+if (args.version) {
+    console.log(`jf ${pkg.version}`);
     Deno.exit();
 }
 
